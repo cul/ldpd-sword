f8ec0320967edbaec9b2baab24fdfd19ebedafdd
Fred Duby <fcd1@columbia.edu>
2016-07-25 16:32:42 -0400

    Disallow null for name in migration file for depositor

--- db/migrate/20160725203012_create_depositors.rb
+++ db/migrate/20160725203012_create_depositors.rb
@@ -1,7 +1,7 @@
 class CreateDepositors < ActiveRecord::Migration
   def change
     create_table :depositors do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
