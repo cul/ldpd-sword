abc885665a402d4efdd606899995f3463420ce47
Fred Duby <fcd1@columbia.edu>
2016-07-25 16:21:23 -0400

    Add new columns to store header values and remove resulting duplicate column

--- db/migrate/20160725191209_create_deposits.rb
+++ db/migrate/20160725191209_create_deposits.rb
@@ -4,11 +4,17 @@ class CreateDeposits < ActiveRecord::Migration
       t.belongs_to :depositor, index: true, foreign_key: true
       t.belongs_to :collection, index: true, foreign_key: true
       t.string :title
-      t.string :on_behalf_of
       t.string :item_in_hyacinth
       t.datetime :embargo_release_date
       t.integer :status, null: false, index: true, default: 0
       t.text :errors
+      t.string :header_content_type
+      t.string :header_content_md5
+      t.string :header_user_agent
+      t.string :header_content_disposition_filename
+      t.string :header_x_on_behalf_of
+      t.boolean :header_x_verbose, default: false
+      t.boolean :header_x_no_op, default: false
 
       t.timestamps null: false
     end
