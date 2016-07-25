c832d5ec0e27768ea1ed49b9ee51690e7d24fc14
Fred Duby <fcd1@columbia.edu>
2016-07-25 14:52:25 -0400

    Disallow null name for collection in migration file

--- db/migrate/20160725184253_create_collections.rb
+++ db/migrate/20160725184253_create_collections.rb
@@ -1,7 +1,7 @@
 class CreateCollections < ActiveRecord::Migration
   def change
     create_table :collections do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
