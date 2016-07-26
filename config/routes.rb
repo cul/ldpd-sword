378962a6d16619e864ce0ffc8cb095ef7c30b273
Fred Duby <fcd1@columbia.edu>
2016-07-26 16:08:03 -0400

    Add resources :depositors

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :collections  
+  resources :collections
+  resources :depositors
 
   # Example resource route with options:
   #   resources :products do
