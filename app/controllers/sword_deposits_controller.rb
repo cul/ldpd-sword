97a66e116f6316bfac9a03f2b0b22b2cdb9d82fe
Fred Duby <fcd1@columbia.edu>
2016-07-26 17:07:52 -0400

    Add title to sword_deposit white list, and change form and index view

--- app/controllers/sword_deposits_controller.rb
+++ app/controllers/sword_deposits_controller.rb
@@ -69,6 +69,6 @@ class SwordDepositsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sword_deposit_params
-      params.fetch(:sword_deposit, {})
+      params.fetch(:sword_deposit, {}).permit(:title)
     end
 end
