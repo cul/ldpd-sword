d5f5deade5992b93403ec7057096e43896fdda50
Fred Duby <fcd1@columbia.edu>
2016-07-25 20:13:56 -0400

    Add quick test case, uncover fixture problems

--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -4,4 +4,12 @@ class CollectionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @collection = Collection.new(name: "Example Collection")
+  end
+
+  test "should be valid" do
+    assert @collection.valid?
+  end
 end
