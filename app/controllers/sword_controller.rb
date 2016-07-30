64f29f6ad428e394466db8d0e5969a6f73fed3da
Fred Duby <fcd1@columbia.edu>
2016-07-30 16:57:51 -0400

    Add more functionality and tests

--- app/controllers/sword_controller.rb
+++ app/controllers/sword_controller.rb
@@ -2,6 +2,7 @@ require "sword/deposit_request"
 
 class SwordController < ApplicationController
   before_action :check_for_valid_collection_slug, only: [:deposit]
+  before_action :check_basic_http_authentication, only: [:deposit]
 
   def deposit
     # puts request.inspect if Rails.env.development? or Rails.env.test?
@@ -22,7 +23,11 @@ class SwordController < ApplicationController
     end
 
     def check_basic_http_authentication
-
+      result = false
+      @user_id, @password = Sword::DepositRequest.pullCredentials(request)
+      @depositor = Depositor.find_by(basic_authentication_user_id: @user_id)
+      result = (@depositor.basic_authentication_password == @password) unless @depositor.nil?
+      head 511 unless result
     end
 end
 
--- test/controllers/sword_controller_test.rb
+++ test/controllers/sword_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SwordControllerTest < ActionController::TestCase
 
-  def setup_auth
+  def setup_auth(bad_passwd = false)
     # config = YAML.load_file(fixture_path_for('sword/config.yml'))
     # Deposits::Sword::SwordTools.instance_variable_set(:@all_config,config)
     @user_id = depositors(:one).basic_authentication_user_id
@@ -12,7 +12,12 @@ class SwordControllerTest < ActionController::TestCase
     # m['name'] == @user
     # end
     # testuser['password'] = @password
-    b64 = Base64.encode64("#{@user_id}:#{@password}")
+    # b64 = Base64.encode64("#{@user_id}:#{@password}")
+    if bad_passwd
+      b64 = Base64.encode64("#{@user_id}:#{@password}foo")
+    else
+      b64 = Base64.encode64("#{@user_id}:#{@password}")
+    end
     @auth_header = "Basic #{b64}".strip
   end
 
@@ -21,6 +26,20 @@ class SwordControllerTest < ActionController::TestCase
     @request.env['HTTP_AUTHORIZATION'] = @auth_header
     post :deposit, collection_slug: 'collection-one'
     assert_response :success
+  end
+
+  test "should post deposit, failure, unknown collection slug" do
+    setup_auth
+    @request.env['HTTP_AUTHORIZATION'] = @auth_header
+    post :deposit, collection_slug: 'collection-on'
+    assert_response 400
+  end
+
+  test "should post deposit, authentication failure" do
+    setup_auth true
+    @request.env['HTTP_AUTHORIZATION'] = @auth_header
+    post :deposit, collection_slug: 'collection-one'
+    assert_response 511
   end
 
 end
