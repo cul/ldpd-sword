c281114f1786839a5bdafd3697caaedae3c7d5fa
Fred Duby <fcd1@columbia.edu>
2016-07-30 12:51:56 -0400

    Add basic authentication credentials to Depositor in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@
 
 Collection.create(name: "A Collection", slug: "a-collection")
 Collection.create(name: "Another Collection",slug: "another-collection")
+
+Depositor.create(name: "First Depositor",
+                 basic_authentication_user_id: "firstdid",
+                 basic_authentication_password: "firstdpasswd")
+
+Depositor.create(name: "Second Depositor",
+                 basic_authentication_user_id: "seconddid",
+                 basic_authentication_password: "secondpasswd")
+
