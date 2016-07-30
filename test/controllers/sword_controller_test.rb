e7a2eda916e212329a464bdfdc5536ba3f0aaef8
Fred Duby <fcd1@columbia.edu>
2016-07-30 15:44:01 -0400

    Add lib/deposits/sword/deposit.rb from hypatia-new
    Renamed it to lib/sword/deposit_request.
    Made initial changes to SwordController#deposit to use it

--- test/controllers/sword_controller_test.rb
+++ test/controllers/sword_controller_test.rb
@@ -1,12 +1,25 @@
 require 'test_helper'
 
 class SwordControllerTest < ActionController::TestCase
-  test "should get deposit" do
-    get :deposit, collection_slug: 'fff'
-    assert_response :success
+
+  def setup_auth
+    # config = YAML.load_file(fixture_path_for('sword/config.yml'))
+    # Deposits::Sword::SwordTools.instance_variable_set(:@all_config,config)
+    @user_id = depositors(:one).basic_authentication_user_id
+    @password = depositors(:one).basic_authentication_user_id
+    # @password = rand(10000).to_s(16)
+    # testuser = Deposits::Sword::SwordTools.getUserConfig.detect do |m|
+    # m['name'] == @user
+    # end
+    # testuser['password'] = @password
+    b64 = Base64.encode64("#{@user_id}:#{@password}")
+    @auth_header = "Basic #{b64}".strip
   end
+
   test "should post deposit" do
-    post :deposit, collection_slug: 'fff'
+    setup_auth
+    @request.env['HTTP_AUTHORIZATION'] = @auth_header
+    post :deposit, collection_slug: 'collection-one'
     assert_response :success
   end
 
