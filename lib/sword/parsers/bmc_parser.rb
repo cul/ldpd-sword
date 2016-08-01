3a42c3fca3d980a383b24eada12852a17c298957
Fred Duby <fcd1@columbia.edu>
2016-08-01 14:49:42 -0400

    Add original parsing code from hypatia-new

---
+++ lib/sword/parsers/bmc_parser.rb
@@ -0,0 +1,170 @@
+require 'rake'
+require 'nokogiri'
+require 'deposits/deposit_utils'
+require 'deposits/deposit_content'
+require 'deposits/person'
+module Deposits
+class BmcParser
+  
+  @@TYPE_OF_CONTENT = 'text'
+  @@GENRE = 'Articles'
+  @@LANGUAGE = 'English'
+  
+  @deposit_content = nil
+  @zip_file = nil
+  
+  attr_accessor :deposit_content 
+                
+  def parse(temp_dir, zip_file)
+    
+    @zip_file = zip_file 
+    
+    @deposit_content = DepositContent.new
+    
+    parse_content(@deposit_content, temp_dir)
+    
+    return @deposit_content
+  end    
+  
+
+  def parse_content(deposit_content, content_dir)  
+    
+    deposit_content.type_of_content = @@TYPE_OF_CONTENT
+    deposit_content.genre = @@GENRE
+    deposit_content.language = @@LANGUAGE
+    
+    contentFileName = getContentFileName(content_dir) 
+    contentXml = Nokogiri::XML(File.read(content_dir + "/" + contentFileName))
+    
+    deposit_content.title = contentXml.css("art>fm>bibl>title>p").text
+    deposit_content.abstract = getAbstract(contentXml)
+    
+    deposit_content.source = contentXml.css("art>fm>bibl>source").text
+    deposit_content.issn = contentXml.css("art>fm>bibl>issn").text
+    deposit_content.pubdate = contentXml.css("art>fm>bibl>pubdate").text
+    deposit_content.volume = contentXml.css("art>fm>bibl>volume").text
+    deposit_content.issue = contentXml.css("art>fm>bibl>issue").text   
+    deposit_content.fpage = contentXml.css("art>fm>bibl>fpage").text
+    deposit_content.pub_doi = contentXml.css("art>fm>bibl>xrefbib>pubidlist>pubid[@idtype='doi']").text
+    
+    deposit_content.copyrightNotice = contentXml.css("art>fm>cpyrt>note").text
+
+    deposit_content.authors = getAuthors(contentXml)
+    
+    deposit_content.attachments = getAttachments(content_dir)
+    
+    deposit_content.embargo_code = '0'
+    
+    start_date = DateTime.now
+    deposit_content.embargo_start_date = start_date.strftime('%m/%d/%Y')
+    
+    deposit_content.note = keywords(contentXml)
+
+    return deposit_content
+  end  
+  
+  def keywords(contentXml)
+    
+    keywords = ''
+    contentXml.css("kwdg>kwd").each do |keyword|
+      if(keywords == '')
+        keywords = keyword.text
+      else
+        keywords = keywords + ', ' + keyword.text
+      end    
+    end
+
+    return keywords
+  end
+  
+  def getAllFilesList(content_dir)
+    mets = Nokogiri::XML(File.read(content_dir + '/mets.xml'))
+    files = []
+    mets.css("FLocat[@LOCTYPE='URL']").each do |file_node|
+      file = file_node["href"]
+      if(file == nil)
+        file = file_node["xlink:href"]
+      end
+      files.push(file)
+    end
+    return files   
+  end
+    
+    
+  def getContentFileName(content_dir)       
+    files = getAllFilesList(content_dir)
+    return files[0]
+  end
+   
+    
+  def getDocsList(content_dir)
+    files = getAllFilesList(content_dir)  
+    files = files.drop(1)  
+    return files
+  end   
+  
+  
+  def getAttachments(content_dir)
+    
+      file_list = FileList.new(content_dir + "/*").exclude('**/mets.xml')
+
+      attachments = []
+      
+      file_list.each  do | file |
+        attachments.push(file)
+      end
+      
+      attachments.push(@zip_file)
+
+      return attachments
+  end  
+  
+  
+  def getAuthors(contentXml)
+    
+    authors = []
+    
+    contentXml.css("art>fm>bibl>aug>au").each do |author|
+      person = Person.new
+      person.last_name = author.css("snm").text
+      person.first_name = author.css("fnm").text
+      person.role = 'author'
+      authors.push(person)
+    end     
+    
+    return authors
+  end
+
+
+  def getAbstract(contentXml)
+
+    abstract = ''
+    
+    contentXml.css("art>fm>abs>sec>sec").each do |sec|
+      abstract = getParagraphs(sec, abstract)
+    end    
+    
+    if(abstract == '')
+      contentXml.css("art>bdy>sec").each do |sec|
+        abstract = getParagraphs(sec, abstract)
+      end       
+    end  
+
+    return abstract
+  end  
+  
+  def getParagraphs(sec, abstract)
+    sec.css("p").each_with_index do |p, index|
+      if(index == 0)
+        if(!p.text.empty?)
+          abstract = abstract + p.text + ": "
+        end  
+      else
+        abstract = abstract + p.text + " "
+      end    
+    end 
+    return abstract
+  end
+  
+end
+end
---
+++ lib/sword/parsers/proquest_parser.rb
@@ -0,0 +1,160 @@
+require 'rake'
+require 'nokogiri'
+require 'deposits/deposit_utils'
+require 'deposits/deposit_content'
+require 'deposits/person'
+module Deposits
+class ProquestParser
+  NS = {
+    'mets' =>"http://www.loc.gov/METS/",
+    'etds' => "http://www.etdadmin.com/ns/etdsword",
+    'epdcx'=>"http://purl.org/eprint/epdcx/2006-11-16/"
+  }
+  DEFAULT_NODE = Nokogiri::XML("<foo></foo>").css("foo").first
+
+  TYPE_OF_CONTENT = 'text'
+  GENRE = 'Dissertations'
+  LANGUAGE = 'English'
+  PHYSICAL_LOCATION = 'NNC'
+  RECORD_CONTENT_SOURCE = 'NNC'
+  LANGUAGE_OF_CATALOGING = 'eng'
+  DEGREE = 'Ph.D.'
+  DEGREE_GRANTOR = 'Columbia University'
+  CORPORATE_ROLE = 'originator'
+  ADVISOR_ROLE = 'thesis advisor'
+  AUTHOR_ROLE = 'author'
+  
+  @deposit_content = nil
+  
+  attr_accessor :deposit_content 
+                
+  def parse(temp_dir, zip_file)
+    
+    @deposit_content = DepositContent.new
+    
+    parse_content(@deposit_content, temp_dir)
+    
+    return @deposit_content
+  end  
+
+  def findDataFile(directory)
+      Dir.glob(directory + "/mets.xml") { | file |
+        return file
+      }
+  end
+  
+  def parse_content(deposit_content, content_dir)
+    
+    dataFile = findDataFile(content_dir)
+    contentXml = nil
+    open(dataFile) do |b|
+      contentXml = b.read
+      contentXml.sub!('xmlns="http://www.etdadmin.com/ns/etdsword"', '')
+      # substitute CRNL for NL
+      contentXml.gsub!(/\x0D(\x0A)?/,0x0A.chr)
+      # replace all control characters but NL and TAB
+      contentXml.gsub!(/[\x01-\x08]/,'')
+      contentXml.gsub!(/[\x0B-\x1F]/,'')
+      contentXml = Nokogiri::XML(contentXml)
+    end
+    
+    deposit_content.type_of_content = TYPE_OF_CONTENT
+    deposit_content.genre = GENRE
+    deposit_content.language = LANGUAGE
+    deposit_content.physicalLocation = PHYSICAL_LOCATION
+    deposit_content.recordContentSource = RECORD_CONTENT_SOURCE
+    deposit_content.languageOfCataloging = LANGUAGE_OF_CATALOGING
+    deposit_content.title = (contentXml.css("DISS_title").first || DEFAULT_NODE).text
+    deposit_content.abstract = (contentXml.css("DISS_content>DISS_abstract").first || DEFAULT_NODE).text
+    deposit_content.subjects = getSubjects(contentXml)
+    deposit_content.dateIssued = (contentXml.css("DISS_description>DISS_dates>DISS_comp_date").first || DEFAULT_NODE).text
+    deposit_content.corparate_name = getAffiliation(contentXml)
+    deposit_content.corparate_role = CORPORATE_ROLE
+    
+    deposit_content.authors = getAuthors(contentXml)
+    deposit_content.advisors = getAdvisors(contentXml)
+    deposit_content.attachments = getAttachments(content_dir)
+    deposit_content.embargo_code = (contentXml.css("DISS_submission").first || DEFAULT_NODE)["embargo_code"]
+    if contentXml.at_css("DISS_authorship>DISS_author>DISS_contact>DISS_contact_effdt")
+      deposit_content.embargo_start_date = contentXml.css("DISS_authorship>DISS_author>DISS_contact>DISS_contact_effdt").first.text
+    end
+    if contentXml.at_css("DISS_restriction>DISS_sales_restriction")
+      deposit_content.sales_restriction_date = (contentXml.css("DISS_restriction>DISS_sales_restriction").first || DEFAULT_NODE)["remove"].to_s
+    end
+    
+  end
+  
+  def getAttachments(content_dir)
+    
+      file_list = FileList.new(content_dir + "/*").exclude('**/mets.xml', '**/*DATA.xml')
+
+      attachments = []
+      
+      file_list.each  do | file |
+        attachments.push(file)
+      end
+      
+      return attachments
+  end
+  
+  def getAffiliation(contentXml)
+    
+    affiliation = (contentXml.css("DISS_description>DISS_institution>DISS_inst_name").first || DEFAULT_NODE).text
+    affiliation = affiliation + ". " + (contentXml.css("DISS_description>DISS_institution>DISS_inst_contact").first || DEFAULT_NODE).text
+  end  
+  
+  def getAdvisors(contentXml)
+    
+    advisors = []
+    
+    contentXml.css("DISS_description>DISS_advisor").each do |advisor|
+      
+      person = Person.new
+      
+      person.last_name = advisor.css("DISS_surname").text
+      person.first_name = advisor.css("DISS_fname").text
+      person.middle_name = advisor.css("DISS_middle").text
+      person.affiliation = getAffiliation(contentXml)
+      person.role = ADVISOR_ROLE
+      
+      advisors.push(person)
+    end     
+    
+    return advisors
+  end    
+ 
+  def getAuthors(contentXml)
+    
+    authors = []
+    
+    contentXml.css("DISS_authorship>DISS_author").each do |author|
+      person = Person.new
+      
+      person.last_name = author.css("DISS_surname").text
+      person.first_name = author.css("DISS_fname").text
+      person.middle_name = author.css("DISS_middle").text
+      person.affiliation = getAffiliation(contentXml)
+      person.role = AUTHOR_ROLE 
+      person.degree = DEGREE
+      person.degree_grantor = DEGREE_GRANTOR
+      
+      authors.push(person)
+    end     
+    
+    return authors
+  end   
+ 
+  def getSubjects(contentXml)
+     
+    subjects = []
+    
+    contentXml.css("DISS_description>DISS_categorization>DISS_category").each do |category|
+      subjects.push(category.css("DISS_cat_desc").text)
+    end     
+    
+    return subjects
+  end  
+  
+
+end
+end
