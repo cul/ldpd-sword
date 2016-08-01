2254946647a4b646b82c4cd66540b32abb84814f
Fred Duby <fcd1@columbia.edu>
2016-08-01 16:15:31 -0400

    Add lib/sword/deposit_utils.rb from hypatia-new
    Currently, only using two methods from this file. Might want to
    rename this file, or move the methods into another existing file.

---
+++ lib/sword/deposit_utils.rb
@@ -0,0 +1,63 @@
+require "deposits/sword"
+require 'zip/zipfilesystem'
+require 'zip/zip'
+module Deposits
+module DepositUtils
+  
+  def self.unpackZip(zipFile, destinationPath)
+    
+    Zip::ZipFile.open(zipFile) { |zip|
+      zip.each { |file|
+        filePpath=File.join(destinationPath, file.name)
+        FileUtils.mkdir_p(File.dirname(filePpath))
+        zip.extract(file, filePpath) unless File.exist?(filePpath)
+      }
+    }
+  end
+  
+  def self.removeDir(directory)
+    FileUtils.remove_dir(directory, force = false)
+  end
+  
+  def self.removeDir(directory, trash_dir)
+    
+    Rails.logger.info "============= directory: " + directory
+    Rails.logger.info "============= trash_dir: " + trash_dir
+    
+   FileUtils.remove_dir(directory, force = true) # This method ignores StandardError if force is true.
+   FileUtils.remove_dir(trash_dir, force = true) 
+   if(File.directory?(directory))
+    FileUtils.mv(directory, trash_dir)
+   end
+  end  
+  
+  
+  
+  def self.getExportedPid(item_id)      
+
+    exports = []
+    conditions = ["item_id = ? AND options.value = ?",item_id,'Aggregator']
+    Export.find(:all, :conditions => conditions, :include => :options).each do |exp|
+      exp.options.each do |op|
+        exports << [op.name, exp.created_at]
+      end
+    end
+
+    return exports
+  end
+  
+  def self.getExportedPids(item_id)   
+       
+    exports = []
+    conditions = {:item_id => item_id}
+    Export.find(:all, :conditions => conditions, :include => :options).each do |exp|
+      exp.options.each do |op|
+        exports << [op.name, op.value, exp.created_at]
+      end
+    end
+    exports.sort! {|a,b| a[1] <=> b[1]}
+    return exports
+  end  
+
+end
+end
