f0c56f5ffa55bba8d62fb04f3f770c63d44a962a
Fred Duby <fcd1@columbia.edu>
2016-08-01 13:37:11 -0400

    Remove code made redundant by check_for_valid_collection_slug

--- app/controllers/sword_controller.rb
+++ app/controllers/sword_controller.rb
@@ -6,11 +6,6 @@ class SwordController < ApplicationController
 
   def deposit
     # puts request.inspect if Rails.env.development? or Rails.env.test?
-    @collection = Collection.find_by slug: params[:collection_slug]
-    if @collection.nil? 
-      # change this logic. Can't remember if rubocop likes return mid-method
-      head :bad_request and return
-    end
     # get authorization info
     puts Sword::DepositRequest.new(request, @collection.slug).inspect
   end
