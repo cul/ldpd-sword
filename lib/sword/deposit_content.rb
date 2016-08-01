79fb42244cf95e1f4c11c1121019a93921a68bc3
Fred Duby <fcd1@columbia.edu>
2016-08-01 15:19:25 -0400

    Add original deposit_content.rb from hypatia-new

---
+++ lib/sword/deposit_content.rb
@@ -0,0 +1,68 @@
+module Deposits
+  class DepositContent
+    DATE_PATTERN = '%Y-%m-%d'
+    attr_accessor :title,
+                  :abstract,
+                  :subjects,
+                  :genre,
+                  :source,
+                  :language,
+                  :physicalLocation,
+                  :recordContentSource,
+                  :languageOfCataloging,
+                  :issn,
+                  :pubdate,
+                  :dateIssued,
+                  :volume,
+                  :issue,
+                  :fpage,
+                  :ezid_doi,
+                  :pub_doi,
+                  :copyrightNotice,
+                  :type_of_resource,
+                  :type_of_content,
+                  :authors,
+                  :advisors,
+                  :corparate_name,
+                  :corparate_role,
+                  :attachments,
+                  :embargo_start_date,
+                  :embargo_code,
+                  :sales_restriction_date,
+                  :note
+    def embargo_release_date
+      DepositContent.getEmbargoDate(self)
+    end
+    def self.getEmbargoDate(deposit_content)
+
+      embargo_release_date = nil
+
+      start_date = Date.parse(deposit_content.embargo_start_date)
+
+      if(deposit_content.embargo_code == '1')
+        embargo_release_date = (start_date + 6.month)
+      end
+
+      if(deposit_content.embargo_code == '2') 
+        embargo_release_date = (start_date + 1.year)
+      end
+      
+      if(deposit_content.embargo_code == '3')
+        embargo_release_date = (start_date + 2.year)
+      end
+      
+      if(deposit_content.embargo_code == '0')
+        embargo_release_date = start_date
+      end
+
+      if(deposit_content.embargo_code == '4')
+        if deposit_content.sales_restriction_date
+          embargo_release_date =
+            Date.parse(deposit_content.sales_restriction_date)
+        end
+      end
+
+      return  embargo_release_date ? embargo_release_date.strftime(DATE_PATTERN) : ''
+    end
+  end
+end
