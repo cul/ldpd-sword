7cc3f8395661883a16bfb52cfe522935aa20f967
Fred Duby <fcd1@columbia.edu>
2016-08-02 12:38:13 -0400

    Flesh out the test for the unzipping of files

--- test/unit/sword/deposit_utils_test.rb
+++ test/unit/sword/deposit_utils_test.rb
@@ -3,12 +3,24 @@ require 'sword/deposit_utils'
 
 class DepositUtilsTest < ActiveSupport::TestCase
   include ActionDispatch::TestProcess
+
   setup do
-    test_file = fixture_file_upload('/zip_files/test.zip')
-    Sword::DepositUtils.unpackZip(test_file, 'tmp/')
+    @test_file = fixture_file_upload('/zip_files/test.zip')
+    @unzip_dir = 'tmp/'
+    @path_expected_file_pdf = File.join(@unzip_dir, 'test.pdf')
+    @path_expected_file_txt = File.join(@unzip_dir, 'test.txt')
   end
 
-  test "assert true" do
-    assert true
+  teardown do
+    FileUtils.rm(@path_expected_file_pdf) if File.exist?(@path_expected_file_pdf)
+    FileUtils.rm(@path_expected_file_txt) if File.exist?(@path_expected_file_txt)
+  end
+
+  test "files correctly unziped" do
+    assert_not File.exist? @path_expected_file_pdf
+    assert_not File.exist? @path_expected_file_txt
+    Sword::DepositUtils.unpackZip(@test_file, @unzip_dir)
+    assert File.exist? @path_expected_file_pdf
+    assert File.exist? @path_expected_file_txt
   end
 end
