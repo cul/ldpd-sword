b99317050a840504128f896aa9ebd9c995358a8c
Fred Duby <fcd1@columbia.edu>
2016-08-21 11:05:40 -0400

    Change location of unzip dir

--- config/initializers/sword.rb
+++ config/initializers/sword.rb
@@ -1,2 +1,6 @@
 SWORD_CONFIG = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/sword.yml")[Rails.env])
+# unzip_dir = "#{Rails.root}/#{SWORD_CONFIG[:unzip_dir]}"
+unzip_dir =  Rails.root.join(SWORD_CONFIG[:unzip_dir])
+puts unzip_dir
+FileUtils.mkdir_p(unzip_dir) unless File.directory?(unzip_dir)
 puts SWORD_CONFIG.inspect if Rails.env.development?
