3cdfeb2801554db7849f305deab1bdaaa5091ce4
Fred Duby <fcd1@columbia.edu>
2018-08-23 12:30:25 -0400

    Add AcademicCommonsEndpoint to handle incoming deposits from AC

---
+++ lib/sword/endpoints/academic_commons_endpoint.rb
@@ -0,0 +1,66 @@
+module Sword
+  module Endpoints
+    class AcademicCommonsEndpoint < MetsToHyacinthEndpoint
+
+      attr_reader :mods_parser,
+                  :hyacinth_adapter
+
+      def initialize(collection, depositor)
+        super
+        @mods_parser = Sword::Parsers::ModsParser.new
+      end
+
+      def handle_deposit(path_to_contents)
+        # handle_deposit from parent class will process the mets portion of the mets.xml file
+        super
+
+        # read mods from mets.xml file and process metadata
+        @mods_parser.parse @mets_parser.xmlData_as_nokogiri_xml_element.first
+        process_metadata
+
+        # @adapter_item_identifier stores the parent pid returned
+        # from Hyacinth
+        @adapter_item_identifier = ingest_item_into_hyacinth
+
+        ingest_documents_into_hyacinth
+        ingest_mets_xml_file_into_hyacinth
+      end
+
+      # This method takes the parsed metadata and maps it to the associated
+      # field in HyacinthIngest object
+      def process_metadata
+        @hyacinth_adapter.abstract = @mods_parser.abstract
+        @hyacinth_adapter.date_issued_start = @mods_parser.date_issued_start
+        @hyacinth_adapter.doi = @mods_parser.identifier_doi
+        @hyacinth_adapter.license_uri =
+          @mods_parser.access_condition_use_and_reproduction_license_uri
+        @hyacinth_adapter.note_type = 'internal'
+        @hyacinth_adapter.note_value = @mods_parser.note_internal
+        process_name_metadata
+        @hyacinth_adapter.title = @mods_parser.title
+        @hyacinth_adapter.uri = @mods_parser.identifier_uri
+        @hyacinth_adapter.use_and_reproduction_uri =
+          @mods_parser.access_condition_use_and_reproduction_rights_status_uri
+        @deposit_title = @mods_parser.title
+      end
+
+      def process_name_metadata
+        @mods_parser.names.each do |mods_name|
+          case mods_name.type
+          when 'personal'
+            individual = Sword::Metadata::NamedEntity.new
+            individual.type = 'personal'
+            individual.role = 'author'
+            # The <namePart> content from AC will always be in the
+            # following format: 'Last, First M.'
+            individual.full_name_naf_format = mods_name.name_part
+            individual.uni = mods_name.id
+            @hyacinth_adapter.names << individual
+          else
+            raise 'AC should only be sending personal names'
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/sword/endpoints/academic_commons_endpoint_spec.rb
@@ -0,0 +1,150 @@
+require 'rails_helper'
+
+RSpec.describe Sword::Endpoints::AcademicCommonsEndpoint do
+  ########################################## hierarchy
+  describe 'hierarchy' do
+    it 'inherits from Endpoint' do
+      expect(described_class.superclass).to eq(Sword::Endpoints::MetsToHyacinthEndpoint)
+    end
+  end
+
+  ########################################## Initial state
+  describe 'Initial state' do
+    context '#initialize' do
+      it 'sets @mods_parser to a new instance of ModsParser' do
+        expect(described_class.new(Collection.new,
+                                   Depositor.new).mods_parser).to be_an_instance_of(Sword::Parsers::ModsParser)
+      end
+
+      it 'sets @hyacinth_adapter to a new instance of HyacinthAdapter' do
+        expect(described_class.new(Collection.new,
+                                   Depositor.new).hyacinth_adapter).to be_an_instance_of(Sword::Adapters::HyacinthAdapter)
+      end
+    end
+  end
+
+  ########################################## #handle_deposit
+  describe '#handle_deposit' do
+    context 'given contents directory as its argument' do
+      before(:context) do
+        @ac_endpoint = Sword::Endpoints::AcademicCommonsEndpoint.new(Collection.new,
+                                                                     Depositor.new)
+        @content_dir = Rails.root.join 'spec/fixtures/test_dirs/ac_contents'
+        # for test purposes, turn off actual sending of POST request
+        @ac_endpoint.hyacinth_adapter.no_op_post = true
+        @collection = Collection.new
+        @depositor = Depositor.new
+      end
+
+      it '#handle_deposit is called successfully (without crashing)' do
+        @ac_endpoint.handle_deposit(@content_dir)
+      end
+    end
+  end
+
+  ########################################## #process_name_metadata
+  describe '#process_name_metadata' do
+    before (:context) do
+      @ac_endpoint = Sword::Endpoints::AcademicCommonsEndpoint.new(Collection.new,
+                                                                   Depositor.new)
+      first_mods_name_personal = Sword::Metadata::ModsName.new
+      first_mods_name_personal.name_part = 'Smith, John C.'
+      first_mods_name_personal.type = 'personal'
+      first_mods_name_personal.id = 'jcs1'
+      second_mods_name_personal = Sword::Metadata::ModsName.new
+      second_mods_name_personal.name_part = 'Doe, Jane A.'
+      second_mods_name_personal.type = 'personal'
+      second_mods_name_personal.id = 'jad1'
+      @ac_endpoint.mods_parser.names << first_mods_name_personal
+      @ac_endpoint.mods_parser.names << second_mods_name_personal
+      @ac_endpoint.process_name_metadata
+    end
+
+    context 'given @mods_parser populated with 2 test individuals' do
+      it 'info stored in @hyacinth_adapter is correct for first individual' do
+        expect(@ac_endpoint.hyacinth_adapter.names.first.type).to eq('personal')
+        expect(@ac_endpoint.hyacinth_adapter.names.first.full_name_naf_format
+              ).to eq('Smith, John C.')
+        expect(@ac_endpoint.hyacinth_adapter.names.first.role).to eq('author')
+        expect(@ac_endpoint.hyacinth_adapter.names.first.uni).to eq('jcs1')
+      end
+
+      it 'info stored in @hyacinth_adapter is correct for second individual' do
+        expect(@ac_endpoint.hyacinth_adapter.names.second.type).to eq('personal')
+        expect(@ac_endpoint.hyacinth_adapter.names.second.full_name_naf_format
+              ).to eq('Doe, Jane A.')
+        expect(@ac_endpoint.hyacinth_adapter.names.second.role).to eq('author')
+        expect(@ac_endpoint.hyacinth_adapter.names.second.uni).to eq('jad1')
+      end
+    end
+  end
+
+  ########################################## #process_metadata
+  describe '#process_metadata' do
+    before (:context) do
+      @ac_endpoint = Sword::Endpoints::AcademicCommonsEndpoint.new(Collection.new,
+                                                                   Depositor.new)
+      @ac_endpoint.mods_parser.abstract = 'This is a sample terse abstract'
+      @ac_endpoint.mods_parser.date_issued_start = '2015'
+      @ac_endpoint.mods_parser.identifier_doi = 'doi:1234234'
+      @ac_endpoint.mods_parser.identifier_uri = 'http://sampleuri'
+      @ac_endpoint.mods_parser.note_internal = 'Sample note to catalogers.'
+      @ac_endpoint.mods_parser.title = 'This is a Sample Title'
+      @ac_endpoint.mods_parser.access_condition_use_and_reproduction_license_uri =
+        'https://creativecommons.org/licenses/by/4.0/'
+      @ac_endpoint.mods_parser.access_condition_use_and_reproduction_rights_status_uri =
+        'http://rightsstatements.org/vocab/InC/1.0/'
+
+      first_mods_name_personal = Sword::Metadata::ModsName.new
+      first_mods_name_personal.name_part = 'Smith, John C.'
+      first_mods_name_personal.type = 'personal'
+      first_mods_name_personal.id = 'jcs1'
+      second_mods_name_personal = Sword::Metadata::ModsName.new
+      second_mods_name_personal.name_part = 'Doe, Jane A.'
+      second_mods_name_personal.type = 'personal'
+      second_mods_name_personal.id = 'jad1'
+      @ac_endpoint.mods_parser.names << first_mods_name_personal
+      @ac_endpoint.mods_parser.names << second_mods_name_personal
+
+      @ac_endpoint.process_metadata
+    end
+
+    context 'given a populated ModsParser instance in @mods_parser' do
+      it 'sets @hyacinth_adapter.abstract correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.abstract).to eq('This is a sample terse abstract')
+      end
+
+      it 'sets @hyacinth_adapter.date_issued_start correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.date_issued_start).to eq('2015')
+      end
+
+      it 'sets @hyacinth_adapter.doi correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.doi).to eq('doi:1234234')
+      end
+
+      it 'sets @hyacinth_adapter.note_type correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.note_type).to eq('internal')
+      end
+
+      it 'sets @hyacinth_adapter.note_value correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.note_value).to eq('Sample note to catalogers.')
+      end
+
+      it 'sets @hyacinth_adapter.title correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.title).to eq('This is a Sample Title')
+      end
+
+      it 'sets @hyacinth_adapter.uri correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.uri).to eq('http://sampleuri')
+      end
+
+      it 'sets @hyacinth_adapter.license_uri correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.license_uri).to eq('https://creativecommons.org/licenses/by/4.0/')
+      end
+
+      it 'sets @hyacinth_adapter.use_and_reproduction_uri correctly' do
+        expect(@ac_endpoint.hyacinth_adapter.use_and_reproduction_uri).to eq('http://rightsstatements.org/vocab/InC/1.0/')
+      end
+    end
+  end
+end
